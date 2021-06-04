cc2e4a8f475ecc2709398be8ac8d0810304f0cf5
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-06-04 20:18:31 +0300

    python

--- bank.py
+++ bank.py
@@ -1,14 +1,52 @@
-class Bank:
-    def __init__(self,accountName,accountNumber,owner):
-        self.accountBalance=accountName
-        self.accountNumber=accountNumber
-        self.owner=owner
-    def withdraw(self):   
-        return f"Hello {self.owner} your  withdraw amount is below the limit"
-    def deposit(self):
-        return f"The{self.onwer} deposit money from{self.accountName}"
-    def  balance(self):
-        return f"Hello please enter {self.accountName} to check your balance"
+class Account:
+    def __init__(self,name,phone):
+        self.name=name
+        self.phone=phone
+        self.balance=0
+        self.transaction=100
+        self.limit=5000
+        self.fees=5
+        self.loan_limit=0
+    def deposit(self,amount): 
+        if amount<=0:
+            return f"please deposit a invalid amount"
+        else:
+            self.balance+=amount
+            return f"Hello {self.name} you have deposited {amount} your new balance is {self.balance}"    
+    def withdraw(self,withdraw_amount):
+        total= withdraw_amount+self.transaction
+        if withdraw_amount<=0:
+            return f"Input valid amount"
+        elif total>self.balance:
+            return f" insufficient balance"
+        else:
+            self.balance=total
+        return f"Hello{self.name} you have sufficient that you can now borrow {withdraw_amount}  and your balance is {self.balance} " 
+
+    def borrow(self,amount):
+        if amount>=self.loan_limit:
+          return f"you will not be able to borrow the money" 
+        elif amount<0:
+            return f"you can't borrow"
+        elif self.loan>0:
+            return f"you have an already existing loan "  
+        else:
+            loan_fee=amount*0.05
+            self.loan=loan_fee+amount 
+            return f"Hello {self.name} you have received {amount} and you pay {self.loan} "  
+
 
 
        
+
+
+    
+   
+
+
+       
+
+        
+    
+
+
--- car.py
+++ car.py
@@ -1,9 +1,11 @@
 class Car:
-    def __init__(self,carColor,carMileage,carModel):
-        self.carColor=carColor
-        self.carMileage=carMileage
-        self.carModel=carModel
+    def __init__(self,car_color,car_mileage,car_model):
+        self.color=car_color
+        self.mileage=car_mileage
+        self.Model=car_model
     def carMileage(self):
-        return f"The carColor {self.carColor},has a mileage of {self.carMileage}"
+        return f"The carColor {self.car_color},has a mileage of {self.car_mileage}"
     def carModel(self):
-        return f"My {self.carModel},has a speed of 180km/hr"
+        return f"My {self.car_model},has a speed of 180km/hr"
+
+        
