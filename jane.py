051bcca9216bb82eaed7a9dce31dc3ab46a6d19a
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-06-17 10:42:58 +0300

    python3

--- jane.py
+++ jane.py
@@ -1,19 +1,54 @@
-class Account:
-    def __init__(self,name,phone):
-      self.name=name
-      self.phone=phone
-      self.balance=0
-    def  borrow(self,amount):
-        if amount<=0:
-            return f"Entered invalid input"
-        elif amount>=self.loan:
-            return f"Your loan is cleared and your account balance is{self.balance}" 
-        else:
-            amount<=self.loan
-            amount-self.loan+=self.loan
-            return f"your account loan_balance is {self.balance}"
+# class Account:
+#     def __init__(self,name,phone):
+#       self.name=name
+#       self.phone=phone
+#       self.balance=0
+#     def  borrow(self,amount):
+#         if amount<=0:
+#             return f"Entered invalid input"
+#         elif amount>=self.loan:
+#             return f"Your loan is cleared and your account balance is{self.balance}" 
+#         else:
+#             amount<=self.loan
+#             amount-self.loan+=self.loan
+#             return f"your account loan_balance is {self.balance}"
 
     
+# class Rename:
+#     def __init__(self,name) :
+#         self.name=name
+
+#         print: Hello my name is f{'name'}
+class School:
+    number_of_students=0
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+        self.fee=50000
+        self.excess=0
+
+        School.number_of_students +=1
+    def calculate_fees(self,amount):
+        try:
+            amount+"five thousands"
+        except TypeError:
+            return "please enter amount in figures"  
+        if amount>500 :
+            return f"You can pay the fees"  
+        elif amount< self.fee and amount>500:
+            return f"your fee balance is {self.fee}" 
+        else:
+            amount>self.fee
+            balance=amount-self.fee
+            self.excess+=self.excess
+            return f"Your amount balance is {self.balance }"
+
+
+                  
+
+
+
+
 
         
                    
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from bank import Account, MobileMoneyAccount
+# from bank import Account, MobileMoneyAccount
 
 # bank = Account(name="Jane",phone="0789765433")
 # print(bank.deposit(50000))
@@ -6,8 +6,21 @@ from bank import Account, MobileMoneyAccount
 # # print(bank.transactions)
 # bank.get_statement()
 
-bank = MobileMoneyAccount(name="Jane Doe",phoneNumber="0789765433",service_provider="safaricom")
-print(bank.deposit(10000))
-print(bank.buy_airtime (1000))
-print(bank.get_statement())
+# bank = MobileMoneyAccount(name="Jane Doe",phoneNumber="0789765433",service_provider="safaricom")
+# print(bank.deposit(10000))
+# print(bank.buy_airtime (1000))
+# print(bank.get_statement())
 # bank.get_statement()
+from jane import School
+ 
+student1=School("paul",4563738,7000)
+student2=School("Mack",332323,4000)
+student3=School(name="Joy",id=454689,fee_balance=2000)
+student4=School(name="Bien",id=59874,fee_balance=1000)
+print(School.number_of_students)
+print(student1.name)
+print(student3.id)
+print(student1.fee_balance)
+print(student2.fee_balance)
+print(student3.fee_balance)
+print(student4.fee_balance)
